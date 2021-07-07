aaa8cfee58228f12d265ecf61dbba7314551c37d
Ilya <i.shaburov@promo-bot.ru>
2021-07-07 16:45:15 +0500

    Усложнение примера Robox

--- python/examples/servo_plot.py
+++ python/examples/servo_plot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 
@@ -13,37 +13,38 @@ import matplotlib.pyplot as plt
 import Servo
 import time
 import bus_handler
+from threading import Timer
 
-#Порт шины данных. По умолчпнию /dev/RS_485
+# Порт шины данных. По умолчпнию /dev/RS_485
 port        = '/dev/RS_485'
 
-#Адрес сервопривода. По умолчанию 10
+# Адрес сервопривода. По умолчанию 10
 servo_id    = 10
 
-#Инициализация шины передачи данных
+# Инициализация шины передачи данных
 master = bus_handler.Bus(port = port, baudrate = 460800, debug = False, timeout = 1.0)
 
-#Инициализация сервопривода
+# Инициализация сервопривода
 servo = Servo.Servo(servo_id, master.bus)
 
-#Желаемая позиция сервопривода
+# Желаемая позиция сервопривода
 set_point   = 4000
 
-#Интервал(сек.) между чтением данных с сервопривода
+# Интервал(сек.) между чтением данных с сервопривода
 t_step      = 0.01
 
-#Общее время (сек) построения графика
+# Общее время (сек) построения графика
 plot_time = 2.0
 
-#Время (сек) от начала построения графика для перемещения сервопривода в желаемую позицию
+# Время (сек) от начала построения графика для перемещения сервопривода в желаемую позицию
 move_start_time = 0.3
 
-#Переменные для графика
+# Переменные для графика
 x = []
 y_position = []
 y_setpoint = []
 
-#Инициализация графика
+# Инициализация графика
 fig = plt.figure()
 data = fig.add_subplot(111)
 data.set_xlabel('Time')
@@ -51,32 +52,47 @@ data.set_ylabel('Servo_value')
 data.set_title('Servo step response')
 data.autoscale(enable=True, axis='both', tight=None)
 
+#  Включим сервопривод, выставим вал в начальное положение
+servo.set_torque(1)
+servo.set_point(0)
 
-#Запуск таймера 
+# Дождемся выставления вала в ноль с некоторой погрешностью +- 15 тиков
+while not (-15 <= servo.get_data()["Position"] <= 15):
+    time.sleep(0.01)
+
+# Запуск таймера 
 start = time.time()
 
-#Строим график пока не прошло выделенное для этого время
-while (time.time() - start) < plot_time:
+# Список точек, точка состоит из 2х чисел, время и желаемое положение
+points = [
+    (0.3,   4000),
+    (0.75,  4500),
+    (1,     3500),
+    (1.15,  4000),
+    (1.30,  3500),
+    (1.5,   4000),
+]
 
-    #Чтение данных с сервопривода
+# Запускает выставление положения через заданный промежуток времени
+for t, p in points:
+    Timer(t, servo.set_point, (p,), kwargs=None).start()
+    
+while (time.time() - start) < plot_time:
+    # Чтение данных с сервопривода
     vals = servo.get_data()
 
-    #Если прошло достаточно времени для старта сервопривода, то отправляем его в желаемую позицию
-    if  (time.time() - start) > move_start_time:
-        #Включение питания обмоток мотора
-        servo.set_torque(1)
-        #Движение в желаемую позицию
-        servo.set_point(set_point)
-
-    #Отображение полученных от сервопривода данных на графике    
+    # Запишем время итерации на график    
     x.append(time.time() - start)
-    #Отображение двух данных от сервопривода: текущей позиции вала двигателя и желаемой позиции
+
+    # Отображение двух данных от сервопривода: текущей позиции вала двигателя и желаемой позиции
     y_position.append(vals["Position"])
     y_setpoint.append(vals["Setpoint"])    
     time.sleep(t_step)
 
+# Выключим сервопривод
+servo.set_torque(0)
 
-#Вывод графика на экран
+# Вывод графика на экран
 data.plot(x, y_position,color="blue",label="Position")
 data.plot(x, y_setpoint,color="red",label="Setpoint")
 data.legend()
