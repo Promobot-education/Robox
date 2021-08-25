8eab6b5459cd56b4665ae7f52350c34662e254cd
Ilya <i.shaburov@promo-bot.ru>
2021-08-25 19:31:51 +0500

    Правки для совместимости с RRStudio

--- python/Ranger.py
+++ python/Ranger.py
@@ -15,22 +15,24 @@ import serial
 import modbus_tk
 import modbus_tk.defines as cst
 from modbus_tk import modbus_rtu
+import time
 
 
-#Ranger registers
-_TRIG_REGISTER      = 20
-_RESULT_REGISTER    = 21
-_COMMAND_REGISTER   = 19
+# Ranger registers
+_TRIG_REGISTER = 20
+_RESULT_REGISTER = 21
+_COMMAND_REGISTER = 19
 
-_MIN_DIST_REGISTER  = 5
+_MIN_DIST_REGISTER = 5
 
 
-#Allowed commands
-_PASS_COMMANDS      = [0x0,0xDEAD]
+# Allowed commands
+_PASS_COMMANDS = [0x0, 0xDEAD]
 
-#Allowed distance for                       
-_MAX_LED_DIST       = 2000
-_MIN_LED_DIST       = 0
+# Allowed distance for
+_MAX_LED_DIST = 2000
+_MIN_LED_DIST = 0
+
 
 class Sensor():
 
@@ -39,11 +41,9 @@ class Sensor():
     Args:
         * master (ModbusRTU): объект посдеовательного порта`.
         * addr (int): Адрес устройства. 1-250
-        
     """
 
-    def __init__(self,addr,master):
-
+    def __init__(self, addr, master):
         self.master = master
         self.addr = addr
         self.logger = modbus_tk.utils.create_logger("console")
@@ -51,15 +51,15 @@ class Sensor():
     #######PRIVATE FUNCTIONS#######
 
     def except_decorator(fn):
-        def wrapped(self,*args):
+        def wrapped(self, *args):
             try:
-                return fn(self,*args)
+                return fn(self, *args)
             except Exception as e:
                 self.logger.error("%s", e)
                 return False
         return wrapped
 
-    ############################ 
+    ############################
 
     @except_decorator
     def trig_sensor(self):
@@ -72,15 +72,13 @@ class Sensor():
             * False если при отправке команды произошла ошибка
         Raises:
             None
-
         """
-        
         return self.master.execute(self.addr, cst.WRITE_SINGLE_REGISTER, _TRIG_REGISTER, output_value=1)
 
-
-    @except_decorator
     def read_sensor(self):
-        """Чтение данных (измеренная дистанция) с датчика
+        """Считать данные из регистров расстояния (измеренная дистанция) с датчика расстояния
+           P.S. стоит учесть, что данные в регистрах изменяются 
+           только после команды на измерение расстояния - trig_sensor()
 
         Args:
             None
@@ -88,26 +86,43 @@ class Sensor():
             Словарь c ключами: 
                 | "IR_distance" 
                 | "US_distance"
+                или пустой, при ошибке
         Raises:
             None
-
-        """           
+        """
         data = {}
+        try:
+            values = self.master.execute(1, cst.READ_HOLDING_REGISTERS, _RESULT_REGISTER, 2)
+        except Exception:
+            return data
 
-        values = self.master.execute(1, cst.READ_HOLDING_REGISTERS, _RESULT_REGISTER, 2)
-        
         data["IR_distance"] = values[0] / 10
         data["US_distance"] = values[1]
 
-        data["IR_distance"] = 255 if data["IR_distance"] == 819 else data["IR_distance"] 
-        data["US_distance"] = 255 if data["US_distance"] == 8100 else data["US_distance"]   
+        data["IR_distance"] = 255 if data["IR_distance"] == 819 else data["IR_distance"]
+        data["US_distance"] = 255 if data["US_distance"] == 8100 else data["US_distance"]
 
         return data
-        
 
+    def measure_once(self):
+        """Измерить расстояние один раз (получить актуальные данные)
 
-    #@except_decorator
-    def set_min_dist(self,dist):
+        Args:
+            None
+        Returns:
+            Словарь c ключами: 
+                | "IR_distance" 
+                | "US_distance"
+                или пустой, при ошибке
+        Raises:
+            None
+        """
+        if self.trig_sensor():
+            time.sleep(0.01)
+            return self.read_sensor()
+
+    @except_decorator
+    def set_min_dist(self, dist):
         """Выставление порога измеренного расстояния, ниже которого загорается светодиод
 
         Args:
@@ -117,9 +132,8 @@ class Sensor():
             * False если при отправке команды произошла ошибка
         Raises:
             None
-
-        """          
-        if dist > _MIN_LED_DIST and dist <= _MAX_LED_DIST:
+        """
+        if _MIN_LED_DIST <= dist <= _MAX_LED_DIST:
             return self.master.execute(self.addr, cst.WRITE_SINGLE_REGISTER, _TRIG_REGISTER, output_value=dist)
         else:
-            raise ValueError("Wrong value for min dist!")     
+            raise ValueError("Wrong value for min dist!")
